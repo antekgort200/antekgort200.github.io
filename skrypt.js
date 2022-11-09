6859ba9e04ac74439991f8053f7fbdfc9bd429f7
antekgort200 <81635525+antekgort200@users.noreply.github.com>
2022-11-09 22:22:52 +0100

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,8 +1,25 @@
 const btn = document.getElementById('btn');
+const games = document.getElementById("g");
+games.addEventListener('click',zmien);
+var press = false;
+function zmien(){
+	if(!press){
+	document.getElementById("gaz").innerHTML += `
+	<h2>Quake</h2>
+	<p>I love all Quake games from classic Quake 1 to newest Quake Champions they have this special thing in them.</p>
+	<h2>Ultrakill</h2>
+	<p>There are two kinds of pepole those who played it and thoes who don't i am one who played and not disapointed</p>
+	<h2>Borderlands series</h2>
+	<p>I have a boner when i see small gold bean on ground and like claptrap</p>
+	<h2>Hedgewars</h2>
+	<p>Funny hedhoges kill themselfs(never played worms).</p>
+		`;
+	press = true;
+	}
+}
 
 function start(){
-	alert("brawo you can press a button");
-	dpcument.getElementById("cont").textContent = "TEST";
+	document.getElementById("spam").innerHTML += "<p>fork :)</p><br>";
 }
 function time(){
 	document.getElementById("click").textContent = "Made in HTML, CSS and JS.";
@@ -16,6 +33,3 @@ function time(){
 
 }
 
-$.getJSON("https://api.ipfy.org?format=json",function(data){
-	alert(data.ip);
-},"json")
